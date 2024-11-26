936bb0aba4394b3ed0c7198c4441cbafb068d377
Javier Alejandro Conde <157633278+Jconde08@users.noreply.github.com>
2024-11-25 23:07:53 -0500

    Add files via upload

---
+++ Taller 6/Taller6-JavierConde-Codigo/src/uniandes/dpoo/swing/interfaz/agregar/PanelBotonesAgregar.java
@@ -0,0 +1,63 @@
+package uniandes.dpoo.swing.interfaz.agregar;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class PanelBotonesAgregar extends JPanel implements ActionListener
+{
+    /**
+     * El comando utilizado para el botón que sirve para crear un nuevo restaurante
+     */
+    private static final String CREAR = "nuevo";
+
+    /**
+     * El comando utilizado para el botón que sirve para cerrar la ventana sin crear un restaurante
+     */
+    private static final String CERRAR = "ver";
+
+    private JButton butNuevo;
+    private JButton butCerrar;
+
+    /**
+     * La ventana principal de la aplicación
+     */
+    private VentanaAgregarRestaurante ventanaPrincipal;
+
+    public PanelBotonesAgregar( VentanaAgregarRestaurante ventanaPrincipal )
+    {
+        this.ventanaPrincipal = ventanaPrincipal;
+
+        setLayout( new FlowLayout( ) );
+
+     // TODO completar
+        butNuevo = new JButton("Nuevo");
+        butNuevo.setActionCommand("NUEVO");
+        butNuevo.addActionListener(this);
+        add(butNuevo);
+
+        // TODO completar
+        butCerrar = new JButton("Cancelar");
+        butCerrar.setActionCommand("CANCELAR");
+        butCerrar.addActionListener(this);
+        add(butCerrar);
+    }
+
+    @Override
+    public void actionPerformed( ActionEvent e )
+    {
+        String comando = e.getActionCommand( );
+        if( comando.equals( CREAR ) )
+        {
+            ventanaPrincipal.agregarRestaurante( );
+        }
+        else if( comando.equals( CERRAR ) )
+        {
+            ventanaPrincipal.cerrarVentana( );
+        }
+    }
+}
